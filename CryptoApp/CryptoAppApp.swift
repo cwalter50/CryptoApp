cd47054f904b08c464235f7dabd9ec84c33937a1
Chris Walter <walt50@gmail.com>
2022-04-14 19:25:57 -0400

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Christopher Walter on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
