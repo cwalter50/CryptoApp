736de6baba0076fb0b24cfbcb42612ad558352ae
Chris Walter <walt50@gmail.com>
2022-04-24 20:47:57 -0400

    Added Networking Manager to reuse a lot of the code in the API call to CoinGecko. Will pay off big down the road.

--- CryptoApp/Services/CoinDataService.swift
+++ CryptoApp/Services/CoinDataService.swift
@@ -24,30 +24,14 @@ class CoinDataService {
     {
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        // Download Data using Combine. The teacher thinks it is the future of iOS Programming. Very powerful
-        
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { (output) -> Data in
-                guard let response = output.response as? HTTPURLResponse, response.statusCode >= 200 && response.statusCode < 300 else {
-                    throw URLError(.badServerResponse)
-                }
-                return output.data
-                
-            }
-            .receive(on: DispatchQueue.main)
+        // Download Data using Combine. The teacher thinks it is the future of iOS Programming. Very powerful. A lot of the code for this has been refractored and put into static functions in NetworkingManager
+        coinSubscription = NetworkingManager.download(url: url)
             .decode(type: [CoinModel].self, decoder: JSONDecoder())
-            .sink { completion in
-                switch completion {
-                case .finished :
-                    break
-                case .failure(let error):
-                    print(error.localizedDescription)
-                }
-            } receiveValue: { [weak self] (returnedCoins) in
+            .sink(receiveCompletion: NetworkingManager.handleCompletion, receiveValue: { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
                 self?.coinSubscription?.cancel()
-            }
+            })
+            
             
 
                 
